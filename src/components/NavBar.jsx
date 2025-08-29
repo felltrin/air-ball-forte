0c516d29c168273be5c3e7571e98616871ad62f4
Lucius Holzinger <luciholzinger@gmail.com>
2025-08-29 08:53:13 -0700

    the navbar styling is aligned with the intended design
    Signed-off-by: Lucius Holzinger <luciholzinger@gmail.com>

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,32 @@
+import { Button, Flex, Stack, Text } from "@chakra-ui/react";
+
+const NavBar = () => {
+  return (
+    <Stack minH={"100vh"}>
+      <Flex alignItems={"center"} justifyContent={"space-between"} p={6}>
+        <Flex>
+          <Text fontWeight={"bold"} textStyle={"3xl"}>
+            Aether
+          </Text>
+        </Flex>
+        <Flex gap={3}>
+          <Text textStyle={"xl"}>Features</Text>
+          <Text textStyle={"xl"}>About Us</Text>
+          <Text textStyle={"xl"}>Contact</Text>
+        </Flex>
+        <Flex>
+          <Button
+            size={"xl"}
+            bg={"#ED4F01"}
+            textStyle={"xl"}
+            _hover={{ bg: "#000" }}
+          >
+            Get Started
+          </Button>
+        </Flex>
+      </Flex>
+    </Stack>
+  );
+};
+
+export default NavBar;
