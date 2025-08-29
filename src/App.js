7b62edf744e35a437f3d833eacfc6cc059206888
Lucius Holzinger <luciholzinger@gmail.com>
2025-08-29 08:55:19 -0700

    the main file now shows the updated styling
    Signed-off-by: Lucius Holzinger <luciholzinger@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import { Button, Flex, Stack, Text, Theme } from "@chakra-ui/react";
+import { Theme } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <Theme appearance="light">
-      <Stack minH={"100vh"}>
-        <Flex
-          alignItems={"center"}
-          justifyContent={"space-between"}
-          px={3}
-          py={3}
-        >
-          <Text>Aether</Text>
-          <Flex gap={3}>
-            <Text>Features</Text>
-            <Text>About us</Text>
-            <Text>Contact</Text>
-          </Flex>
-          <Button>Get Started</Button>
-        </Flex>
-      </Stack>
+      <NavBar />
     </Theme>
   );
 }
